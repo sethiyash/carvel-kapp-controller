b5edee3c03093b468d2144095f9755aa4bdde5ca
sethiyash <yashsethiya97@gmail.com>
2023-01-10 21:27:12 +0530

    restrict exposing values in KC package

--- hack/build-release.sh
+++ hack/build-release.sh
@@ -8,7 +8,7 @@ mkdir -p .imgpkg/
 # makes the get_kappctrl_ver function available (scrapes version from git tag)
 source $(dirname "$0")/version-util.sh
 
-ytt -f config/ -f config-release -v kapp_controller_version="$(get_kappctrl_ver)" --data-values-env=KCTRL | kbld --imgpkg-lock-output .imgpkg/images.yml -f- > ./tmp/release.yml
+ytt -f config/ -f config-release -v dev.kapp_controller_version="$(get_kappctrl_ver)" --data-values-env=KCTRL | kbld --imgpkg-lock-output .imgpkg/images.yml -f- > ./tmp/release.yml
 
 shasum -a 256 ./tmp/release.yml
 
