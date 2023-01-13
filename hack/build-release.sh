ec8c53b87aa966134eebc35b898ab920d601b2e7
sethiyash <yashsethiya97@gmail.com>
2023-01-13 23:23:28 +0530

    debug args null

--- hack/build-release.sh
+++ hack/build-release.sh
@@ -15,4 +15,6 @@ yq eval '.metadata.annotations."kapp-controller.carvel.dev/version" = env(versio
 ytt -f config/ -f config-release -v dev.kapp_controller_version="$(get_kappctrl_ver)" --data-values-env=KCTRL | kbld --imgpkg-lock-output .imgpkg/images.yml -f- > ./tmp/release.yml
 shasum -a 256 ./tmp/release.yml
 
+cat ./tmp/release.yml
+
 echo SUCCESS
