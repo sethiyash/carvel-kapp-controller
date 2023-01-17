e7aad65eaa4bb3d7129de55e654217e9ef2b56d5
sethiyash <yashsethiya97@gmail.com>
2023-01-17 13:01:29 +0530

    yq eval after ytt

--- hack/build-release.sh
+++ hack/build-release.sh
@@ -10,9 +10,10 @@ source $(dirname "$0")/version-util.sh
 
 export version="$(get_kappctrl_ver)"
 
-yq eval '.metadata.annotations."kapp-controller.carvel.dev/version" = env(version)' -i "config/deployment.yml"
-
 ytt -f config/ -f config-release -v dev.kapp_controller_version="$(get_kappctrl_ver)" --data-values-env=KCTRL | kbld --imgpkg-lock-output .imgpkg/images.yml -f- > ./tmp/release.yml
+
+yq eval '.metadata.annotations."kapp-controller.carvel.dev/version" = env(version)' -i "./tmp/release.yml"
+
 shasum -a 256 ./tmp/release.yml
 
 cat ./tmp/release.yml
