2dff9c9c951ab90f68dfbbc87f6f5b44efb2173a
sethiyash <yashsethiya97@gmail.com>
2023-01-17 13:01:29 +0530

    yq eval after ytt

--- hack/build-release.sh
+++ hack/build-release.sh
@@ -10,9 +10,18 @@ source $(dirname "$0")/version-util.sh
 
 export version="$(get_kappctrl_ver)"
 
-yq eval '.metadata.annotations."kapp-controller.carvel.dev/version" = env(version)' -i "config/deployment.yml"
+sed -ri 's/^(\s*)(kapp-controller.carvel.dev/version\s*:\s*v0.0.0\s*$)/\1kapp-controller.carvel.dev/version: $version/' config/deployment.yml
+
+cat config/deployment.yml
+
+# sed -ri'.bak' -e  's/^(\s*)("kapp-controller.carvel.dev/version"\s*:\s*"v0.0.0"\s*$)/\1"kapp-controller.carvel.dev/version": env(version)/' config/deployment.yml
+
+# sed -ri 's/^(\s*)(image\s*:\s*nginx\s*$)/\1image: apache/' file
 
 ytt -f config/ -f config-release -v dev.kapp_controller_version="$(get_kappctrl_ver)" --data-values-env=KCTRL | kbld --imgpkg-lock-output .imgpkg/images.yml -f- > ./tmp/release.yml
+
+# yq 'select(.kind == "Deployment") | .metadata.annotations."kapp-controller.carvel.dev/version" = env(version)' ./tmp/release.yml
+
 shasum -a 256 ./tmp/release.yml
 
 cat ./tmp/release.yml
